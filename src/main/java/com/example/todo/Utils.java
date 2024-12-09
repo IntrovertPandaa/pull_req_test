0e9701ee9d36f1f858aef55f0eea01a1b4b7da66
ERWIN FORMELOZA <2205487@slu.edu.ph>
2024-12-09 11:28:09 +0800

    added documentation comments

--- src/main/java/com/example/todo/Utils.java
+++ src/main/java/com/example/todo/Utils.java
@@ -5,18 +5,35 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class Utils {
+    /**
+     * Sorts a list of tasks by their ID in ascending order.
+     * 
+     * @param tasks The list of tasks to sort.
+     * @return A new list of tasks sorted by ID.
+     */
     public static List<Task> sortTasksById(List<Task> tasks) {
         return tasks.stream()
                 .sorted(Comparator.comparingInt(Task::getId))
                 .collect(Collectors.toList());
     }
 
+    /**
+     * Filters the list of tasks to include only completed tasks.
+     * 
+     * @param tasks The list of tasks to filter.
+     * @r
     public static List<Task> filterCompletedTasks(List<Task> tasks) {
         return tasks.stream()
                 .filter(Task::isCompleted)
                 .collect(Collectors.toList());
     }
 
+    /**
+     * Filters the list of tasks to include only incomplete tasks.
+     * 
+     * @param tasks The list of tasks to filter.
+     * @return A new list containing only incomplete tasks.
+     */
     public static List<Task> filterIncompleteTasks(List<Task> tasks) {
         return tasks.stream()
                 .filter(task -> !task.isCompleted())
